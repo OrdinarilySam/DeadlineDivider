932c28f19651f24d438ac20c44d2f7bac11c38ce
OrdinarilySam <ordinarilysam@gmail.com>
2022-09-15 22:41:16 -0500

    new value button now summons input field

--- script.js
+++ script.js
@@ -13,12 +13,30 @@ let dom = {
     "amtPerYear": document.getElementById("amtPerYear"),
     "submitBtn": document.getElementById("submitBtn"),
     "todayBtn": document.getElementById("todayBtn"),
-    "newValue": document.getElementById("newValueBtn")
+    "newValue": document.getElementById("newValueBtn"),
+    "valueContainer": document.getElementById("valueContainer")
 }
 let valueFields = []
 
 dom.todayBtn.addEventListener("click", setStartAsToday)
 dom.newValue.addEventListener("click", createNewValue)
+
+dom.valueContainer.addEventListener("click", (event)=>{
+    if(!event.target.nodeName === "BUTTON") return;
+    let buttonId = event.target.id
+    
+    if(buttonId.startsWith("valSubmitBtn")){
+        buttonId = parseInt(buttonId.replace("valSubmitBtn", ""))
+        // Create a new value field
+        // Render the new values
+        valueFields[buttonId].value = document.getElementById(`valueInput${buttonId}`).value
+    }
+    else if(buttonId.startsWith("valDeleteBtn")){
+        // Remove a value field
+        // Render the new values
+
+    }
+})
 
 
 function captureDates(){
@@ -30,11 +48,34 @@ function captureDates(){
 
 function setStartAsToday(){
     dates.startDate = dates.now
+    dom.startDateInput.value = dateFormat(dates.startDate)
 }
 
 function renderFields(){
     // Using the function of value Fields, render them given a new field is added, changed, or removed
     // Will not render when the sumbit button is clicked
+    valueFieldContainerHTML = ""
+    for(let i = 0; i < valueFields.length; i++){
+        if(!valueFields[i].hasAnswered){
+            valueFieldContainerHTML += `
+            <div class="value-field" id="valueField${i}">
+                <input type="number" min="0" placeholder="number" id="valueInput${i}" class="value-input">
+                <button id="valSubmitBtn${i}" class="val-submit-btn">+</button>
+            </div>
+            `
+        }
+    }
+    dom.valueContainer.innerHTML = valueFieldContainerHTML
+
+}
+
+function createNewValue(){
+    newValue = {
+        hasAnswered: false,
+        value: null
+    }
+    valueFields.push(newValue)
+    renderFields()
 }
 
 function calculateDiff(){
@@ -42,29 +83,24 @@ function calculateDiff(){
     return dateDiff
 }
 
-function createNewValue(){
 
-}
-
-
-
-const submitValue = document.getElementById("value-input-container")
-submitValue.addEventListener("click", (event)=>{
-    if(!event.target.nodeName === "BUTTON") return;
-    let buttonId = event.target.id
+dom.submitBtn.addEventListener("click", (event)=>{
     
-    if(buttonId.startsWith("valSubmitBtn")){
-        buttonId = buttonId.replace("valSubmitBtn", "")
-        // Create a new value field
-        // Render the new values
-    }
-    else if(buttonId.startsWith("valDeleteBtn")){
-        // Remove a value field
-        // Render the new values
-
-    }
 })
 
+function dateFormat(inputDate) {
+    const date = new Date(inputDate)
+
+    const day = date.getDate()
+    const month = date.getMonth() + 1
+    const year = date.getFullYear()
+
+    let dateString = year.toString() + "-"
+    dateString += month.toString().padStart(2, "0") + "-"
+    dateString += day.toString()
+
+    return dateString;
+}
 
 // submitBtn.addEventListener("click", function(){
 //     // const startDate = startDateInp.value
