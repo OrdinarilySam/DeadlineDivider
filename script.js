df18d9efe4ae8be18fea992ed5e70e69f30fad17
OrdinarilySam <ordinarilysam@gmail.com>
2022-09-18 21:09:18 -0500

    old comments removed

--- script.js
+++ script.js
@@ -202,36 +202,3 @@ function stuff(ms, total){
     dom.total.textContent = `Total: ${total}`
     return dayDiff
 }
-
-// submitBtn.addEventListener("click", function(){
-//     // const startDate = startDateInp.value
-//     // const endDate = endDateInp.value
-//     // const amt = numberInp.value
-//     // if(!startDate || !endDate || !amt) return
-//     // if(amt<=0) return
-
-//     // const startDateArr = startDate.split("-")
-//     // const endDateArr = endDate.split("-")
-
-//     // const yearDiff = endDateArr[0] - startDateArr[0]
-//     // const monthDiff = endDateArr[1] - startDateArr[1]
-//     // const dayDiff = endDateArr[2] - startDateArr[2]
-
-//     // const totalDiff = (yearDiff*365) + (monthDiff*30) + dayDiff
-//     // const perDay = amt/totalDiff
-
-//     // let perWeek = (perDay*7)
-//     // if(perWeek>amt) perWeek = amt
-//     // else perWeek = perWeek.toFixed(2)
-//     // let perMonth = (perDay*30)
-//     // if (perMonth > amt) perMonth = amt
-//     // else perMonth = perMonth.toFixed(2)
-//     // let perYear = (perDay*365)
-//     // if (perYear > amt) perYear = amt
-//     // else perYear = perYear.toFixed(2)
-
-//     // amtPerDay.textContent = `Per Day: ${perDay.toFixed(2)}`
-//     // amtPerWeek.textContent = `Per Week: ${perWeek}`
-//     // amtPerMonth.textContent = `Per Month: ${perMonth}`
-//     // amtPerYear.textContent = `Per Year: ${perYear}`
-// })
